f6193a83b8c36b72fc5283473e12dc3feb5a5eec
JPLeon18 <juanpabloleonmera@gmail.com>
2019-02-05 15:53:08 -0500

    Se añadio el delete

--- index.php
+++ index.php
@@ -6,6 +6,87 @@ $app = new \Slim\Slim();
 
 $dbconect = new mysqli("localhost","root","","curso_angular4");
 
+
+// ---- LISTAR PRODUCTOS ---------------------------------
+
+$app->get("/products", function () use ($app,$dbconect){
+
+    $query = $dbconect ->query("SELECT * FROM productos ORDER BY id DESC");
+
+    $productos = array();
+    while ($producto = $query ->fetch_assoc()){
+            $productos[]=$producto;
+    }
+
+    $result = array(
+        'status' => 'success',
+        'code' => 200,
+        'data' => $productos
+    );
+
+
+    echo json_encode($result);
+});
+
+
+// ---- ACTUALIZAR DATOS --------------------------------
+
+// ---- DEVOLVER UN PRODUCTO -----------------------------
+
+$app->get("/products/:id",function ($id)use ($app,$dbconect){
+
+    $query = $dbconect->query( 'SELECT * FROM productos WHERE id ='. $id);
+
+    if ($query->num_rows == 1){
+        $producto = $query->fetch_assoc();
+
+        $result = array(
+            'status' => 'success',
+            'code' => 200,
+            'data' => $producto
+        );
+
+    }else{
+        $result = array(
+            'status' => 'Error',
+            'code' => 404,
+            'message' => 'Producto no encontrado'
+        );
+    }
+
+    echo json_encode($result);
+});
+
+
+// ---- ELIMINAR PRODUCTO --------------------------------
+
+$app->get("/delete-product/:id", function ($id)use ($app,$dbconect){
+
+    $query = $dbconect->query('DELETE FROM productos WHERE id ='.$id);
+
+
+    if ($query){
+        $result = array(
+            'status' => 'success',
+            'code' => 200,
+            'message' => 'Producto eliminado correctamente'
+        );
+    }else{
+        $result = array(
+            'status' => 'Error',
+            'code' => 404,
+            'message' => 'Producto no encontrado'
+        );
+    }
+
+    echo json_encode($result);
+
+});
+
+
+
+// ----- GUARDAR PRODUCTO --------------------------------
+
 $app->post("/add_product", function ()use($app, $dbconect) {
 
     $json = $app->request->post('json');
