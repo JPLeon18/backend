7c3a7cf70acfac738fa61adc93c286bff3df2be3
JPLeon18 <juanpabloleonmera@gmail.com>
2019-02-06 15:56:07 -0500

    API Rest finalizada

--- index.php
+++ index.php
@@ -2,9 +2,21 @@
 
 require_once "vendor/autoload.php";
 
+require_once "piramide-uploader/PiramideUploader.php";
+
 $app = new \Slim\Slim();
 
 $dbconect = new mysqli("localhost","root","","curso_angular4");
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+    die();
+}
+
 
 
 // ---- LISTAR PRODUCTOS ---------------------------------
@@ -31,16 +43,36 @@ $app->get("/products", function () use ($app,$dbconect){
 
 // ---- ACTUALIZAR DATOS --------------------------------
 
-$app->post("update-product/:id", function ($id)use ($dbconect, $app){
+$app->post("/update-product/:id", function ($id)use ($dbconect, $app){
 
     $json = $app ->request->post('json');
     $data = json_decode($json, true);
 
     $sql = "UPDATE productos SET ".
         "nombre = '{$data['nombre']}',".
-        "descripcion = '{$data['descripcion']}',".
-        "precio ="
+        "descripcion = '{$data['descripcion']}',";
+    if (isset($data['imagen'])){
+        $sql .= "imagen = '{$data['imagen']}',";
+    }
+        $sql.= "precio ='{$data['precio']}' WHERE id = {$id}";
 
+    $query = $dbconect->query($sql);
+
+    if ($query){
+        $result =array(
+            'status' => 'Success',
+            'code' => 200,
+            'message' => 'Actualizado'
+        );
+    }else{
+        $result = array(
+            'status' => 'Error',
+            'code' => 404,
+            'message' => 'No actualizado'
+        );
+    }
+
+    echo json_encode($result);
 });
 
 // ---- DEVOLVER UN PRODUCTO -----------------------------
@@ -143,5 +175,42 @@ $app->post("/add_product", function ()use($app, $dbconect) {
 
         echo json_encode($result);
 });
+
+
+// GUARDAR UNA IMAGEN ----------------------------------
+
+$app->post("/upload-file", function ()use ($app,$dbconect){
+
+
+    if (isset($_FILES['upload'])){
+
+        $piramideUploader = new PiramideUploader();
+
+        $upload = $piramideUploader->upload('img','upload','uploads', array('image/jpg', 'image/png', 'image/jpeg'));
+
+        $file = $piramideUploader->getInfoFile();
+
+        $file_name = $file['complete_name'];
+
+        if (isset($upload) && $upload["uploaded"] == false){
+            $result = array(
+                'status' => 'Error',
+                'code' => 404,
+                'message' => 'Imagen No subida'
+            );
+        }else{
+            $result = array(
+                'status' => 'Success',
+                'code' => 200,
+                'filename' => $file_name
+            );
+        }
+
+    }
+
+    echo json_encode($result);
+
+});
+
 
 $app->run();
